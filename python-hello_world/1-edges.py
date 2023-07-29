5f5d82740f4aa5f0c2f41b6eeb274d5291803d98
Awash Hedato <awashedato@gmail.com>
2023-07-29 17:08:21 +0300

    updated variable

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -2,3 +2,7 @@ word = "Holberton"
 print("First 3 letters: " + word[0:3])
 print("Last 2 letters: " + word[-2:])
 print("Middle word: " + word[1:8])
+word = "School"
+print("First 3 letters: " + word[0:3])
+print("Last 2 letters: " + word[-2:])
+print("Middle word: " + word[1:6])
