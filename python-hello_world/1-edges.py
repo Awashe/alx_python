3cdd4d73cea51d07b079348871f4d3dbc7f81826
Awash Hedato <awashedato@gmail.com>
2023-07-29 17:05:47 +0300

    1. Copy - Cut - Paste update

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,4 +1,4 @@
 word = "Holberton"
-print(word[0:3])
-print(word[-2:])
-print(word[1:8])
+print("First 3 letters: " + word[0:3])
+print("Last 2 letters: " + word[-2:])
+print("Middle word: " + word[1:8])
