c4e092f624d877af6f108ede9d9868cf22b930c4
Awash Hedato <awashedato@gmail.com>
2023-07-29 18:58:36 +0300

    updated task 3

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -7,6 +7,10 @@ quotient, remainder = divmod(number, 10)
 last_digit = remainder
 if number < 0:
     last_digit = -last_digit
+if number >= 0:
+    last_digit = last_digit
+if number == 0:
+  last_digit = 0
 if last_digit > 5:
     print(f"last digit of {number} is {last_digit} and is greater than 5")
 elif last_digit == 0:
