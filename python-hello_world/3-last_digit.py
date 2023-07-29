255171ddef27c828e3945e0d08d30a7d3186c3fe
Awash Hedato <awashedato@gmail.com>
2023-07-29 18:52:01 +0300

    update task 3

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,7 +1,12 @@
 import random
 number = random.randint(-10000, 10000)
+if number < 0:
+    number = -number
+
 quotient, remainder = divmod(number, 10)
 last_digit = remainder
+if number < 0:
+    last_digit = -last_digit
 if last_digit > 5:
     print(f"last digit of {number} is {last_digit} and is greater than 5")
 elif last_digit == 0:
