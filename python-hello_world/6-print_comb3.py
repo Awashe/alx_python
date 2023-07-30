a26a50fbe653d22e1ef1d6943be5d987fd507ebc
Awash Hedato <awashedato@gmail.com>
2023-07-30 18:50:28 +0300

    task 6 2nd update

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -4,12 +4,3 @@ for i in range(10):
 for i in range(1,9):
     for j in range(i+1,10):
         print("{0}{1}, ".format(i,j), end="")
-print("89")
-'''
-numbers = ""
-for i in range(0, 100):
-  two_digit = "{0:02}".format(i)
-  numbers += two_digit + ", "
-
-print(numbers[:-2])
-'''
