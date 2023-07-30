a0a13bb03244f08983f83975d755589e3300935d
Awash Hedato <awashedato@gmail.com>
2023-07-30 19:00:47 +0300

    3. The last digit udating

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,10 +1,17 @@
 import random
+
 number = random.randint(-10000, 10000)
-print("Last digit of", number, "is", number % 10, "and is", end=" ")
-if number % 10 > 5:
+
+last_digit = abs(number) % 10
+
+if number < 0:
+    last_digit = -last_digit
+
+print("Last digit of {0} is {1} and is".format(number, last_digit), end=" ")
+
+if last_digit > 5:
     print("greater than 5")
-elif number % 10 == 0:
+elif last_digit == 0:
     print("0")
 else:
     print("less than 6 and not 0")
-    
