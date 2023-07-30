6bd490d1f3dc17720f24c55538feefba79e8035a
Awash Hedato <awashedato@gmail.com>
2023-07-30 18:42:47 +0300

    6. Inventing is a combined of brains and material.

---
+++ python-hello_world/6-print_comb3.py
@@ -0,0 +1,7 @@
+for i in range(10):
+    for j in range(i+1,10):
+        print(f"{i}{j}", end=", ")
+for i in range(1,9):
+    for j in range(i+1,10):
+        print(f"{i}{j}", end=", ")
+print("89")
