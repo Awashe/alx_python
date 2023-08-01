db57e0159791f56e08fac57503f15182df3d8314
Awash Hedato <awashedato@gmail.com>
2023-08-01 03:35:43 +0300

    update

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,3 +1,4 @@
+'''
 import sys # for accessing command line arguments
 
 if __name__ == '__main__':
@@ -9,3 +10,16 @@ if __name__ == '__main__':
     print("Number of argument"+("s" if num_args>1 else "")+": "+str(num_args)+(":" if num_args>0 else ".")+"\n")
     for i,arg in enumerate(arg_list):
         print(str(i+1)+": "+arg)
+'''
+import sys
+
+if __name__ == '__main__':
+    num_args = len(sys.argv) - 1
+    arg_list = sys.argv[1:]
+
+    if num_args > 0:
+        print(f"{num_args} argument{'s' if num_args != 1 else ''}:")
+        for i, arg in enumerate(arg_list, start=1):
+            print(f"{i}: {arg}")
+    else:
+        print("No arguments.")
