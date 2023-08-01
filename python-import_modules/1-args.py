9a5c34745878ed26e1cba6ce6babdcf02c2199f4
Awash Hedato <awashedato@gmail.com>
2023-08-01 03:29:31 +0300

    1. How to make a script dynamic!

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,10 @@
+import sys # for accessing command line arguments
+
+# get number of arguments and arguments as a list
+num_args = len(sys.argv) - 1
+arg_list = sys.argv[1:]
+
+# print number of arguments and argument list
+print("Number of argument"+("s" if num_args>1 else "")+": "+str(num_args)+(":" if num_args>0 else ".")+"\n")
+for i,arg in enumerate(arg_list):
+    print(str(i+1)+": "+arg)
