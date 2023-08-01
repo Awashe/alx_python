bc8507a4d8eb5a0b78b4f062a960db6e98161d5a
Awash Hedato <awashedato@gmail.com>
2023-08-01 03:23:21 +0300

    update

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,9 +1,9 @@
+from add_0 import add
+
 a = 1
 b = 2
 
-add_module = __import__('add_0')
-
-result = add_module.add(a, b)
+result = add(a, b)
 
 if __name__ == '__main__':
     print("{} + {} = {}".format(a, b, result))
