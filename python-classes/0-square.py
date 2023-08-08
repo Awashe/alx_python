01ba29ec98c080b16b7abe2bd970f4ee5638c407
Awash Hedato <awashedato@gmail.com>
2023-08-08 05:47:47 +0300

    0. Square with size 1st update

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,13 @@
+"""
+This module provides a Square class that defines a square by its size.
+"""
 class Square:
-    """This class defines a square by its size"""
+    """
+    This class defines a square by its size.
+    """
+
     def __init__(self, size):
-        """ Initializes an instance of the Square class."""
+        """
+        Initializes an instance of the Square class.
+        """
         self.__size = size
