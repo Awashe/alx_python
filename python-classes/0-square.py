2ed1bc0ef1397caf552b7d3ce19c450413d2fda8
Awash Hedato <awashedato@gmail.com>
2023-08-08 05:43:38 +0300

    0. Square with size

---
+++ python-classes/0-square.py
@@ -0,0 +1,5 @@
+class Square:
+    """This class defines a square by its size"""
+    def __init__(self, size):
+        """ Initializes an instance of the Square class."""
+        self.__size = size
