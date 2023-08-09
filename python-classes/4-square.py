20fbdabebbdee82947bf573da0997de7557e3589
Awash Hedato <awashedato@gmail.com>
2023-08-09 04:00:02 +0300

    4. Printing a square 1st update

---
+++ python-classes/4-square.py
@@ -0,0 +1,73 @@
+"""
+This module provides a Square class that defines a square by its size.
+"""
+
+class Square:
+    """
+    This class defines a square by its size.
+    """
+
+    def __init__(self, size=0):
+        """
+        Initializes an instance of the Square class with an optional size parameter.
+
+        Parameters:
+            size (int): The size of the square. Defaults to 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """
+        Gets the size of the square.
+
+        Returns:
+            The size of the square.
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Sets the size of the square.
+
+        Parameters:
+            value (int): The new size of the square.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Computes the area of the square.
+
+        Returns:
+            The area of the square.
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """
+        Prints the square to the standard output using '#' characters.
+
+        Returns:
+            None.
+        """
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__size):
+                for j in range(self.__size):
+                    print("#", end="")
+                print()
