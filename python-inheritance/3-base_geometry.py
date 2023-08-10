9a374aac4f88ec556358675ca8282365aac17ac1
Awash Hedato <awashedato@gmail.com>
2023-08-10 04:02:43 +0300

    3. Geometry module 2nd update

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -3,5 +3,13 @@ class BaseGeometry:
     """
     A base class for geometrical objects.
     """
+
+    def __init__(self):
+        pass
+
     def __dir__(self):
-        return list(self.__dict__.keys())
+        return list(self.__dict__.keys()) + \
+               [attr for attr in dir(type(self)) if not attr.startswith("__")]
+
+    def __repr__(self):
+        return "<{} object at {}>".format(type(self).__name__, hex(id(self)))
