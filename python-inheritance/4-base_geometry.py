99684e87e55472017f624e4c6d93837156cd64e2
Awash Hedato <awashedato@gmail.com>
2023-08-10 04:23:50 +0300

    4. Improve Geometry

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,12 @@
+"""Write a class BaseGeometry (based on 3-base_geometry.py)."""
+class BaseGeometry:
+    """A class representing a geometric shape that can calculate its area."""
+
+    def area(self):
+        """Calculate the area of the geometric shape.
+
+        Raises:
+            Exception: This method is not implemented in this base class.
+
+        """
+        raise Exception("area() is not implemented")
