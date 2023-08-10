af24c12f7e77b5d3ab6d92d9e9b9d3863d4b1259
Awash Hedato <awashedato@gmail.com>
2023-08-10 04:06:44 +0300

    3rd

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,15 +1,6 @@
 """This is an empty class BaseGeometry."""
 class BaseGeometry:
+    pass
     """
     A base class for geometrical objects.
     """
-
-    def __init__(self):
-        pass
-
-    def __dir__(self):
-        return list(self.__dict__.keys()) + \
-               [attr for attr in dir(type(self)) if not attr.startswith("__")]
-
-    def __repr__(self):
-        return "<{} object at {}>".format(type(self).__name__, hex(id(self)))
