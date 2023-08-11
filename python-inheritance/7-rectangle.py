f33ae462ce066d1d806cfd2cf1534a35a3458a5a
Awash Hedato <awashedato@gmail.com>
2023-08-11 12:18:12 +0300

    7. Full rectangle updated

--- python-inheritance/7-rectangle.py
+++ python-inheritance/7-rectangle.py
@@ -1,7 +1,11 @@
-"""Write a class Rectangle that inherits from BaseGeometry (5-base_geometry.py). (task based on 6-rectangle.py)"""
+"""
+Write a class Rectangle that inherits from BaseGeometry (5-base_geometry.py). (task based on 6-rectangle.py)
+"""
 class Rectangle(BaseGeometry):
     """
-    A class representing a Rectangle shape.
+    A class representing a Rectangle shape
+
+    ...
 
     Attributes
     ----------
@@ -12,9 +16,9 @@ class Rectangle(BaseGeometry):
 
     Methods
     -------
-    area():
+    area(self):
         Returns the area of the Rectangle object.
-    __str__():
+    __str__(self):
         Returns a string representation of the Rectangle object.
     """
 
@@ -23,19 +27,17 @@ class Rectangle(BaseGeometry):
         Constructs and initializes a new Rectangle object with the
         specified width and height values.
 
-        Parameters
-        ----------
-        width : int
-            The width of the Rectangle object.
-        height : int
-            The height of the Rectangle object.
+        Parameters:
+        width (int): The width of the Rectangle object.
+        height (int): The height of the Rectangle object.
 
-        Raises
-        ------
-        TypeError
-            If either width or height is not an integer.
-        ValueError
-            If either width or height is less than or equal to 0.
+        Returns:
+        None
+
+        Raises:
+        TypeError: if either width or height is not an integer.
+        ValueError: if either width or height is less than or equal to 0.
+
         """
         self.integer_validator("width", width)
         self.integer_validator("height", height)
@@ -46,10 +48,12 @@ class Rectangle(BaseGeometry):
         """
         Returns the area of the Rectangle object.
 
-        Returns
-        -------
-        int
-            The area of the Rectangle object.
+        Parameters:
+        None
+
+        Returns:
+        The area of the Rectangle object.
+
         """
         return self.__width * self.__height
 
@@ -57,9 +61,24 @@ class Rectangle(BaseGeometry):
         """
         Returns a string representation of the Rectangle object.
 
-        Returns
-        -------
-        str
-            A string representation of the Rectangle object.
+        Parameters:
+        None
+
+        Returns:
+        A string representation of the Rectangle object.
+
         """
         return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def __repr__(self):
+        """
+        Returns a string representation of the Rectangle object.
+
+        Parameters:
+        None
+
+        Returns:
+        A string representation of the Rectangle object.
+
+        """
+        return self.__str__()
