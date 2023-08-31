b9749e0e1190e7c998833139d9c279beb330eefc
Awash Hedato <awashedato@gmail.com>
2023-08-31 05:53:18 +0300

    4th update

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,5 +1,6 @@
 """
 A module that fetches the status of a website using the requests library.
+This code module uses the `requests` library to send an HTTP GET request to a specified URL and checks the status code of the returned response. If the status code is 200, indicating that the request was successful, the code prints the body of the response to the console in a formatted manner. If the status code is not 200, indicating a failure, an error message is printed to the console along with the actual status code returned by the server.
 """
 import requests
 
