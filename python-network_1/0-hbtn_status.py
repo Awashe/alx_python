06803b483020227da6b509907529fcc4fcfd1372
Awash Hedato <awashedato@gmail.com>
2023-08-31 04:24:26 +0300

    0. What's my status? #1

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+import requests
+
+url="https/alu-intranet.htbn.io/status"
+response = requests.get(url)
+
+if response.status_code == 200:
+    print("Body response")
+    print("\t-type:",type(response.text))
+    print("\t-content:", response.text)
+else:
+    print("Error fetcheing website. Status code:, response.status_code")
