1305701ae94c833fa4c8a84543a971b2ea179090
Awash Hedato <awashedato@gmail.com>
2023-08-31 04:41:19 +0300

    1st update

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,19 +1,5 @@
 """
 A module that fetches the status of a website using the requests library.
-
-This module defines a function that sends a GET request to a specified URL and displays
-the body of the response, including its type and content. It uses the requests library,
-so this library must be installed to use the function.
-
-Example:
-    To fetch the status of the website https://alu-intranet.hbtn.io/status, use the
-    following code:
-
-        import status_checker
-
-        url = "https://alu-intranet.hbtn.io/status"
-        status_checker.check_status(url)
-
 """
 import requests
 
