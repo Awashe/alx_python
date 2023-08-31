55cafaf7111c6a064a9b439a2772e3986de6fddf
Awash Hedato <awashedato@gmail.com>
2023-08-31 06:10:12 +0300

    7th update

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,17 +3,23 @@
 The purpose of this code module is to perform a basic status check of a web application by sending an HTTP GET request and checking its response status code. This code can be used standalone or integrated as part of a larger application to perform regular or automated web application checks.
 
 To use this module, the user needs to import the `requests` library and provide a URL to check. The code retrieves and prints the content of the response if the status code is 200, providing useful information for debugging or testing purposes. If the status code is not 200, an error message is printed to the console to indicate the failure."""
-import urllib.request
+import requests
 
-url = 'https://alu-intranet.htbn.io/status'
+def fetch_status():
+    """
+    Fetches the status from https://alu-intranet.hbtn.io/status using requests.
+    Returns a string representation of the response body.
+    """
+    url = 'https://alu-intranet.hbtn.io/status'
+    response = requests.get(url)
+    response_body = response.json()
 
-try:
-    with urllib.request.urlopen(url) as response:
-        if response.getcode() == 200:
-            print("Body response")
-            print("\t-type:", type(response.read().decode('utf-8')))
-            print("\t-content:", response.read().decode('utf-8'))
-        else:
-            print("Error fetching website. Status code:", response.getcode())
-except:
-    print("Could not connect to the URL")
+    status = response_body.get('status')
+    environment = response_body.get('environment')
+
+    body_str = f"Status: {status}\n\
+        Environment: {environment}"
+    return body_str
+
+if __name__ == '__main__':
+    print(fetch_status())
