111f59752f302c2e75bfdf37b148b8a83c5a40d5
Awash Hedato <awashedato@gmail.com>
2023-08-31 06:05:34 +0300

    6th update

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -3,14 +3,17 @@
 The purpose of this code module is to perform a basic status check of a web application by sending an HTTP GET request and checking its response status code. This code can be used standalone or integrated as part of a larger application to perform regular or automated web application checks.
 
 To use this module, the user needs to import the `requests` library and provide a URL to check. The code retrieves and prints the content of the response if the status code is 200, providing useful information for debugging or testing purposes. If the status code is not 200, an error message is printed to the console to indicate the failure."""
-import requests
+import urllib.request
 
-url="https/alu-intranet.htbn.io/status"
-response = requests.get(url)
+url = 'https://alu-intranet.htbn.io/status'
 
-if response.status_code == 200:
-    print("Body response")
-    print("\t-type:",type(response.text))
-    print("\t-content:", response.text)
-else:
-    print("Error fetcheing website. Status code:, response.status_code")
+try:
+    with urllib.request.urlopen(url) as response:
+        if response.getcode() == 200:
+            print("Body response")
+            print("\t-type:", type(response.read().decode('utf-8')))
+            print("\t-content:", response.read().decode('utf-8'))
+        else:
+            print("Error fetching website. Status code:", response.getcode())
+except:
+    print("Could not connect to the URL")
