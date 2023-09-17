c50bbffa6e922df8561dfbf3befd480e134b78a8
Awash Hedato <awashedato@gmail.com>
2023-09-17 08:44:02 +0300

    5. Number template bulk update

--- python-web_framework/5-number_template.py
+++ python-web_framework/5-number_template.py
@@ -45,9 +45,9 @@ def display_number(n):
 def display_number(n):
     return "{} is a number".format(n)
 
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    return render_template('5-number.html', n=n)
+@app.route("/number_template/<int:n>", strict_slashes=False)
+def display_templates(n):
+    return render_template("5-number.html", number=n)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0",port=5000)
