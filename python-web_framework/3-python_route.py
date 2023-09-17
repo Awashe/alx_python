515f3e2e8b17e57cabba2b5be2b3e7c7474adc49
Awash Hedato <awashedato@gmail.com>
2023-09-17 08:07:32 +0300

    3. Python is cool!

--- python-web_framework/3-python_route.py
+++ python-web_framework/3-python_route.py
@@ -25,6 +25,7 @@ def c_text(text):
     text = text.replace("_", " ")
     return "C {}".format(text)
 
+@app.route('/python/', defaults={'text': 'default'})
 @app.route("/python/<text>", strict_slashes=False)
 def python(text):
     # Replace '_' with spaces
